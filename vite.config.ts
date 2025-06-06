ef6718233edf2635edafbdb7addd3e512021a851
Mauro Guimaraes <mauro.guimaraes@SEGNA.COM.BR>
2025-06-06 12:17:47 -0300

    fix: adicionando a base da produção

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // 👈 ESSA LINHA É FUNDAMENTAL PARA DEPLOY
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
